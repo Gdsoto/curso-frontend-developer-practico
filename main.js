b630899fa3237022b64f49a0ace8a78a503ece7c
Gerson Soto <gersonsoto5622@gmail.com>
2022-08-08 12:43:08 -0500

    [Added] shopping Cart

--- main.js
+++ main.js
@@ -2,13 +2,28 @@ const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburgerMenu = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.product-detail');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
 
 const toggleDesktopMenu = () => {
+	if (!menuCartIcon.classList.contains('inactive')) {
+		menuCartIcon.classList.add('inactive');
+	}
 	desktopMenu.classList.toggle('inactive');
 };
 const toggleMobileMenu = () => {
+	if (!menuCartIcon.classList.contains('inactive')) {
+		menuCartIcon.classList.add('inactive');
+	}
 	mobileMenu.classList.toggle('inactive');
+};
+const toggleCartMenu = () => {
+	if (!mobileMenu.classList.contains('inactive')) {
+		mobileMenu.classList.add('inactive');
+	}
+	menuCartIcon.classList.toggle('inactive');
 };
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
 hamburgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleCartMenu);
