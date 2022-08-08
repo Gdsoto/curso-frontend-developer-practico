51dcfb1f4d89b649fbe138999593902b5377e399
Gerson Soto <gersonsoto5622@gmail.com>
2022-08-08 12:08:55 -0500

    [Added] mobile menu and mediaQueries

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburgerMenu = document.querySelector('.menu');
 
 const toggleDesktopMenu = () => {
 	desktopMenu.classList.toggle('inactive');
 };
+const toggleMobileMenu = () => {
+	mobileMenu.classList.toggle('inactive');
+};
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
